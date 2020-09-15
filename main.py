73698921c06c82781350d429bd73a576b6887e38
Grace Lavin <gcl5087@psu.edu>
2020-09-15 13:50:24 +0000

    finsihed 1

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
 # Author: Grace Lavin gcl5087@psu.edu
-# Collaborator: Grace Lavin gcl5087@psu.edu
-# Collaborator: Grace Lavin gcl5087@psu.edu
-# Collaborator: Grace Lavin gcl5087@psu.edu
+# Collaborator: Yue Wu yzw5627@psu.edu
+# Collaborator: Shiao Zhuang sqz5328@psu.edu
+# Collaborator: Michael Artlip mva5905@psu.edu
 # Section: 1
-# Breakout: 12
+# Breakout: 18
+
+def sum_n(n):
+  if n == 0:
+    return 0
+  else:
+    return n + sum_n(n-1)
+  
+
+def print_n(s, n):
+  sum_n(n)
+  if n > 0 :
+    print(f"{s}")
+    n = n -1 
+    print_n(s,n)
+  return 0
+
+
+def run():
+  getN = input("Enter an int: ")
+  intN = int(getN)
+  print(f"sum is {sum_n(intN)}.")
+  getS = input("Enter a string: ")
+  print_n(getS, intN)
+ 
+   
+  
+if __name__ == "__main__":
+  run()
+
+
